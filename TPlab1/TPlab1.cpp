8b71cc8c4319d88d57cfac870e8e21f25ff54ff6
vitalbit <vitalbit@tut.by>
2013-09-18 12:14:21 +0300

    third commit

--- TPlab1/TPlab1.cpp
+++ TPlab1/TPlab1.cpp
@@ -14,10 +14,16 @@ class Apple
 	private:
 		bool ripe;
 		string color;
+		int seeds;
 	public:
 		Apple() {
 			ripe = true;
 			color = "red";
+			seeds = rand() % 11;
+		}
+		int getSeeds()
+		{
+			return seeds;
 		}
 };
 
@@ -25,11 +31,16 @@ class Tree
 {
 	private:
 		vector <Apple> tree;
+		int allSeeds;
 	public:
 		Tree(int n) {
-			if (n>5000) n=5000;
-			for (int i=0; i<n; i++)
+			allSeeds = 0;
+			if (n > 5000) n = 5000;
+			for (int i = 0; i < n; i++)
+			{
 				tree.push_back(*(new Apple()));
+				allSeeds += (tree.end()-1)->getSeeds();
+			}
 		}
 
 		int grow() {
@@ -38,7 +49,10 @@ class Tree
 			x=x-5000*(x/5000);
 			if ((int)tree.size()+x>5000) x=5000-tree.size();
 			for (int i=0; i!=x; i++)
+			{
 				tree.push_back(*(new Apple()));
+				allSeeds += (tree.end()-1)->getSeeds();
+			}
 			return x;
 		}
 
@@ -47,23 +61,30 @@ class Tree
 			x=x-5000*(x/5000);
 			if ((int)tree.size()-x < 0) x=tree.size();
 			for (int i=0; i<x; i++)
+			{
+				allSeeds-=(tree.end()-1)->getSeeds();
 				tree.pop_back();
+			}
 			return x;
 		}
 
 		int count() {
 			return tree.size();
 		}
+
+		int countSeeds() {
+			return allSeeds;
+		}
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	srand(time(NULL));
 	Tree *tr = new Tree(0); 
 	string cmd="";
-	srand(time(NULL));
 	while (cmd!="quite")
 	{
-		cout << "Enter\ngrow  -- for new apples\ncount -- for count of apples\nshake -- for apples down\nquite -- for quite" << endl;
+		cout << "Enter\ngrow  -- for new apples\ncount -- for count of apples\nshake -- for apples down\nseeds -- for count of seeds\nquite -- for quite" << endl;
 		cin >> cmd;
 		if (cmd=="grow") 
 			cout << "Has grown " << tr->grow() << " apples" << endl;
@@ -71,8 +92,14 @@ int _tmain(int argc, _TCHAR* argv[])
 			cout << tr->shake() << " apples have down" << endl;
 		else if (cmd=="count") 
 			cout << "Tree has " << tr->count() << " apples" << endl;
+		else if (cmd=="seeds")
+			cout << "Count of seeds = " << tr->countSeeds() << endl;
 		else if (cmd!="quite") cout << "Wrong command!" << endl;
 	}
 	return 0;
 }
 
+//в €блоко добавить количество косточек
+//commit
+//цвести дереву
+//commit
